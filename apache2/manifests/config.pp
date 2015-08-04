1f3cecc29e0ac656dfad495ad8bad414ab083d41
root <root@master.example.com>
2015-08-04 11:41:35 +0000

    edited init.pp and config.pp added 2 files

--- apache2/manifests/config.pp
+++ apache2/manifests/config.pp
@@ -19,30 +19,25 @@ class apache2::config {
               owner => 'root',
               group => 'root',
               mode =>  750,
-              content => ' <html>
-                           <head> <title>test site</title></head>
-                           <body><h1>Hi ..... site.com</h1></body>
-                           </html>',            
+              source => 'puppet:///modules/apache2/index.html',            
             }
 
-         file {'site.com.conf':
-                require => Package['apache2'],
-                path => '/etc/apache2/sites-available/site.com.conf',
-                #ensure => present,
-                content => '<VirtualHost *:80>
-                ServerAdmin webmaster@site
-                Servername  site
-                DocumentRoot /var/www/html/site
-                ErrorLog ${APACHE_LOG_DIR}/error.log
-                CustomLog ${APACHE_LOG_DIR}/access.log combined
-                </VirtualHost>',
-               notify => Service['apache2'],
-              }
             exec {'enablevhost':
                    path => [ '/bin','/usr/bin','/usr/sbin'],
                    command => 'a2ensite site.com.conf',
                    notify  => Service['apache2'],
                  }
+
+
+
+         file {'site.com.conf':
+                require => Package['apache2'],
+                path => '/etc/apache2/sites-available/site.com.conf',
+                #ensure => present,
+                source =>  'puppet:///modules/apache2/site.com.conf',
+               notify => Service['apache2'],
+              }
+            
             
 }
     
--- apache2/manifests/init.pp
+++ apache2/manifests/init.pp
@@ -1,8 +1,8 @@
 
 class apache2 {
       
-    class {'apache2::install':} ->
-    class {'apache2::config':} ~>
+    class {'apache2::install':} 
+    class {'apache2::config':}
     class {'apache2::service':}
    
               
